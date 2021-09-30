ef0d056296d36d527c29d89ede3649cc74745f0d
matheusifz <matheusifz@gmail.com>
2021-09-30 01:19:39 -0300

    learned logical operators

---
+++ aula22/index.js
@@ -0,0 +1,22 @@
+/* Operadores Lógicos 
+&& -> AND todas as expressões precisam ser verdadeiras para retornar true
+| | -> OR
+! -> NOT
+*/
+
+/* const expressionAnd = true && true && true && true;
+ */
+/* const expressionOr = false || true || true || false;
+ */
+/* 
+const usuario = "Matheus"; // form que o usuário digitou
+const senha = "1234567"; // form que o usuário digitou
+
+//                         True            False
+const vaiLogar = usuario === "Matheus" && senha === "234567"; */
+
+/* console.log(!true);
+ */
+const iloveHoney = !false;
+
+console.log(iloveHoney);
